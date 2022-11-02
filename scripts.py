12844f254628c29da0800d1b8f1d12fa0bad6986
fastfir <95661133+fastfir@users.noreply.github.com>
2022-11-02 11:19:07 -0700

    Added Port Scan

--- scripts.py
+++ scripts.py
@@ -1,3 +1,8 @@
 import os
-def scanip():
+direc = os.path.expanduser("~")
+def scanip(ip):
     os.chdir("/home/fastfir/nmap")
+    os.system("./nmap -oG " + direc + "/scan.txt -sn -n -T4 " + ip + " |grep 'ip' scan.txt")
+    os.system("")
+    os.system("./nmap -oG " + direc + "/ports.txt -Pn -iL hosts.txt")
+
