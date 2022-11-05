a0208d173ba2648d2e114d28fd5506db5509d02b
fastfir <95661133+fastfir@users.noreply.github.com>
2022-11-04 22:58:39 -0700

    Script gets the IPs automatically from nmap scan

--- scripts.py
+++ scripts.py
@@ -1,8 +1,13 @@
 import os
 direc = os.path.expanduser("~")
+hosts = []
 def scanip(ip):
     os.chdir("/home/fastfir/nmap")
-    os.system("./nmap -oG " + direc + "/scan.txt -sn -n -T4 " + ip + " |grep 'ip' scan.txt")
-    os.system("")
+    os.system("./nmap -oG " + direc + "/scan.txt -sn -n -T4 " + ip)
+    with open('scan.txt', 'r') as scan:
+        for line in scan:
+            if "Host:" in line:
+                ip = line.split(" ", 2)
+                hosts.append(ip[1])
     os.system("./nmap -oG " + direc + "/ports.txt -Pn -iL hosts.txt")
 
